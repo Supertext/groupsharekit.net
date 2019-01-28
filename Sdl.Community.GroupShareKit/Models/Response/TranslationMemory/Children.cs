5deaf7678d5f67da60460bdeb55956a11e2b0067
ivan stankovic <ivan@supertext.com>
2019-01-28 15:43:02 +0100

    Add Text and ChildList property in Children model

--- Sdl.Community.GroupShareKit/Models/Response/TranslationMemory/Children.cs
+++ Sdl.Community.GroupShareKit/Models/Response/TranslationMemory/Children.cs
@@ -1,25 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using Sdl.Community.GroupShareKit.Clients;
 
 namespace Sdl.Community.GroupShareKit.Models.Response.TranslationMemory
 {
+    [DataContract]
    public class Children
     {
-        /// <summary>
-        /// Gets or sets the id
-        /// </summary>
-       public string Id { get; set; }
-        /// <summary>
-        /// Gets or sets type
-        /// </summary>
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        [DataMember(Name = "type")]
         public string Type { get; set; }
-        /// <summary>
-        /// Gets or sets metadata
-        /// </summary>
+
+        [DataMember(Name = "metadata")]
         public Metadata Metadata { get; set; }
+
+        [DataMember(Name = "tokens")]
+        public List<Token> Tokens { get; set; }
+
+        [DataMember(Name = "text")]
+        public string Text { get; set; }
+
+        
+        [DataMember(Name= "children")]
+        public List<Children> ChildList { get; set; }
     }
 }
---
+++ Sdl.Community.GroupShareKit/Models/Response/TranslationMemory/Token.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Sdl.Community.GroupShareKit.Models.Response.TranslationMemory
+{
+    public class Token
+    {
+
+        public string Type { get; set; }
+
+        public string Text { get; set; }
+
+    }
+}
